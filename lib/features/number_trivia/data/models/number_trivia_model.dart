4edd3ebc2cec750f935d0386b20c191062e87b87
Ahmed El-Nakib <araffat@ipmagix.com>
2022-07-25 16:42:36 +0200

    Implement Course code

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -8,4 +8,19 @@ class NumberTriviaModel extends NumberTrivia {
     text: text,
     number: number,
   );
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'],
+      // number: json['number'],
+      number: (json['number'] as num).toInt(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
 }
---
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -0,0 +1,72 @@
+import 'package:clean_arch/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_arch/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:clean_arch/features/number_trivia/presentation/widgets/loading_widget.dart';
+import 'package:clean_arch/features/number_trivia/presentation/widgets/message_display.dart';
+import 'package:clean_arch/features/number_trivia/presentation/widgets/trivia_controlls.dart';
+import 'package:clean_arch/features/number_trivia/presentation/widgets/trivia_display.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../injection_container.dart';
+
+class NumberTriviaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Number Trivia'),
+      ),
+      body: SingleChildScrollView(
+        child: buildBody(context),
+      ),
+    );
+  }
+}
+
+BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+  return BlocProvider(
+    builder: (_) => sl<NumberTriviaBloc>(),
+    child: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 10),
+            // Top half
+            BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+              builder: (context, state) {
+                if (state is Empty) {
+                  return const MessageDisplay(
+                    message: 'Start searching!',
+                  );
+                } else if (state is Loading) {
+                  return const LoadingWidget();
+                } else if (state is Loaded) {
+                  return TriviaDisplay(
+                    numberTrivia: state.trivia,
+                  );
+                }else if (state is Error) {
+                  return MessageDisplay(
+                    message: state.message,
+                  );
+                }
+                return const MessageDisplay(
+                  message: 'Start searching!',
+                );
+              },
+            ),
+            const SizedBox(height: 20),
+            // Bottom half
+            TriviaControls(),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+
+
+
+
+
